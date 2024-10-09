83f32d377392580b0e904107a0cd0898640c78cc
thoeurnsothy <141421476+PhonPuthireach@users.noreply.github.com>
2024-10-09 13:07:47 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { database } from './firebase-config.js';
-
-document.getElementById('join-game').onclick = function() {
-    window.location.href = 'enter-game-code.html';
-}
-
-document.getElementById('create-game').onclick = function() {
-    window.location.href = 'create-game.html';
-}
